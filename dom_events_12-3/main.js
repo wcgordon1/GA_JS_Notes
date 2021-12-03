9dca85e6adf4fbd8e7943c25a3f6b6864137cf56
wcgordon1 <wcgordon1@gmail.com>
2021-12-03 11:14:02 -0800

    done w/vscode for today. See more code from 12-3 at https://dom-hw-12-12-23-menu-lab-two-parts.wcgordon11.repl.co

--- dom_events_12-3/main.js
+++ dom_events_12-3/main.js
@@ -2,19 +2,47 @@
 
 console.log('main.js is being loaded by the script tag in html')
 
-/*Here is the common syntax for registering an event listener for a given event:
-element.addEventListener(<event-name>, <callback>, <use-capture>);</use-capture> */
+//Here is the common syntax for registering an event listener for a given event:
+//element.addEventListener(<event-name>, <callback>, <use-capture>);</use-capture></callback>
 
-const h1El = document.querySelector('h1');
+//const h1El = document.querySelector('h1');
 
-h1El.addEventListener('click', function () {
-    console.log('im click on the h1')
-})
+//h1El.addEventListener('click', function () {
+// console.log('im clicking on the h1')
+//})
 
-//logs how many times the botton is clicked in browser console.
+//logs how many times the button is clicked in browser console.
 const btnEl = document.querySelector('button');
-console.log(btnEl);
+console.log(btnEl)
 
-btnEl.addEventListener('click', function () {
-    console.log('button is being clicked')
+btnEl.addEventListener('click', function (event) {
+    // console.log('button is being clicked')
+    // console.log(event)
+
+    // Create a list element
+    const liEl = document.createElement('li');
+    // console.dir(liEl);
+
+    const inputEl = document.querySelector('input');
+    // console.log(inputEl.value) // < - What is inside the input
+    // Put whatever text is in the input inside of the list element that we created
+    // How do I update the text in my liEl tag?
+    liEl.innerText = inputEl.value;
+
+    console.log(liEl)
+    // then we want to put that element (li) at the end of ul tag 
+
+    const ulEl = document.querySelector('ul')
+
+    ulEl.appendChild(liEl);
+
+    // clear the input
+    inputEl.value = ''
+}); // <---- End of the callback function
+
+const ulEl = document.querySelector('ul')
+ulEl.addEventListener('click', function (event) {
+    event.target.style.textDecoration = 'line-through';
 })
+
+const clickThrough = '';
